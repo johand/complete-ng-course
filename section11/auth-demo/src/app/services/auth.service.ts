e2218bc47fadd7ad0b5f3a31f5b9cf8a10a64517
Johan <johand@openmailbox.org>
2017-12-16 23:24:02 -0430

    Showing or hiding elements

--- section11/auth-demo/src/app/services/auth.service.ts
+++ section11/auth-demo/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { JwtHelper, tokenNotExpired } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
@@ -27,7 +28,21 @@ export class AuthService {
   }
 
   isLoggedIn() {
-    return false;
+    return tokenNotExpired();
+
+    // let jwtHelper = new JwtHelper();
+    // let token = localStorage.getItem('token');
+
+    // if(!token)
+    //   return false;
+
+    // let expirationDate = jwtHelper.getTokenExpirationDate(token);
+    // let isExpired = jwtHelper.isTokenExpired(token);
+
+    // console.log('Expiration', expirationDate);
+    // console.log('isExpired', isExpired);
+
+    // return !isExpired;
   }
 }
 
