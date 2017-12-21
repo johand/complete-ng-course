1c25fddee4e69fd7e1fa62d615a2c281663acd05
Johan <johand@openmailbox.org>
2017-12-20 23:46:55 -0430

    Unsubscribing from subscriptions

--- section13/firebase-demo/src/app/app.component.ts
+++ section13/firebase-demo/src/app/app.component.ts
@@ -1,20 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   courses: any[];
+  subscription: Subscription;
 
   constructor(db: AngularFireDatabase) {
-    db.list('/courses')
+    this.subscription = db.list('/courses')
       .valueChanges()
       .subscribe(courses => {
         this.courses = courses;
         console.log(courses);
       });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
