54b036cce50ff0691bc10cb67e9adb100ddc9686
Johan <johand@openmailbox.org>
2017-12-30 20:46:04 -0430

    Keyframes

--- section14/animations-demo/src/app/animations.ts
+++ section14/animations-demo/src/app/animations.ts
@@ -1,4 +1,4 @@
-import { trigger, transition, style, animate, state } from '@angular/animations';
+import { trigger, transition, style, animate, state, keyframes } from '@angular/animations';
 
 export let slide = trigger('slide', [
     transition(':enter', [
@@ -7,7 +7,19 @@ export let slide = trigger('slide', [
     ]),
 
     transition(':leave', [
-        animate('0.5s cubic-bezier(.61, .29, .07, 1.02)', style({ transform: 'translateX(-100%)'} ))
+        animate('0.5s ease-out', keyframes([
+            style({
+                offset: .2,
+                opacity: 1,
+                transform: 'translateX(20px)'
+            }),
+
+            style({
+                offset: 1,
+                opacity: 0,
+                transform: 'translateX(-100%)'
+            })
+        ]))
     ])
 ])
 
