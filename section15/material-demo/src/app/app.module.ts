cba6d76850469ed6ada2291f2b49b49397c64314
Johan <johand@openmailbox.org>
2018-01-08 01:26:38 -0430

    Tooltips

--- section15/material-demo/src/app/app.module.ts
+++ section15/material-demo/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import {
   MatCheckboxModule, MatRadioModule, MatSelectModule, MatInputModule,
   MatDatepickerModule, MatNativeDateModule, MatIconModule, MatButtonModule,
-  MatChipsModule, MatProgressSpinnerModule
+  MatChipsModule, MatProgressSpinnerModule, MatTooltipModule
 } from '@angular/material';
 import { NgModule } from '@angular/core';
 
@@ -29,6 +29,7 @@ import { FormsModule } from '@angular/forms';
     MatButtonModule,
     MatChipsModule,
     MatProgressSpinnerModule,
+    MatTooltipModule,
     FormsModule
   ],
   providers: [],
