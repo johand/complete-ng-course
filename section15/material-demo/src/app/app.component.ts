72c304a228e57b43396832ffa09e2449687dd37b
Johan <johand@openmailbox.org>
2018-01-11 12:11:53 -0430

    Passing data to dialogs

--- section15/material-demo/src/app/app.component.ts
+++ section15/material-demo/src/app/app.component.ts
@@ -6,67 +6,69 @@ import { MatDialog } from '@angular/material';
 import { EditCourseComponent } from './edit-course/edit-course.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // progress = 0;
-  // timer;
-  // isLoading = false;
+    // progress = 0;
+    // timer;
+    // isLoading = false;
 
-  constructor(private dialog: MatDialog) {
-    // this.timer = setInterval(() => {
-    //   this.progress++;
+    constructor(private dialog: MatDialog) {
+        // this.timer = setInterval(() => {
+        //   this.progress++;
 
-    //   if (this.progress === 100) { clearInterval(this.timer); }
-    // }, 20);
+        //   if (this.progress === 100) { clearInterval(this.timer); }
+        // }, 20);
 
-    // this.isLoading = true;
+        // this.isLoading = true;
 
-    // this.getCourses()
-    //   .subscribe(x => this.isLoading = false);
-  }
+        // this.getCourses()
+        //   .subscribe(x => this.isLoading = false);
+    }
 
-  // getCourses() {
-  //   return Observable.timer(2000);
-  // }
+    // getCourses() {
+    //   return Observable.timer(2000);
+    // }
 
-  // title = 'app';
-  // isChecked = true;
+    // title = 'app';
+    // isChecked = true;
 
-  // onChange($event) {
-  //   console.log($event);
-  // }
+    // onChange($event) {
+    //   console.log($event);
+    // }
 
-  // colors = [
-  //   { id: 1, name: 'Red'},
-  //   { id: 2, name: 'Green'},
-  //   { id: 3, name: 'Blue'}
-  // ];
+    // colors = [
+    //   { id: 1, name: 'Red'},
+    //   { id: 2, name: 'Green'},
+    //   { id: 3, name: 'Blue'}
+    // ];
 
-  // color = 2;
+    // color = 2;
 
-  // minDate = new Date(2017, 12, 1);
-  // maxDate = new Date(2018, 1, 6);
+    // minDate = new Date(2017, 12, 1);
+    // maxDate = new Date(2018, 1, 6);
 
-  // categories = [
-  //   { name: 'Begginer' },
-  //   { name: 'Intermediate' },
-  //   { name: 'Advanced' },
-  // ];
+    // categories = [
+    //   { name: 'Begginer' },
+    //   { name: 'Intermediate' },
+    //   { name: 'Advanced' },
+    // ];
 
-  // selectCategory(category) {
-  //   this.categories
-  //     .filter(c => c !== category)
-  //     .forEach(c => c['selected'] = false);
+    // selectCategory(category) {
+    //   this.categories
+    //     .filter(c => c !== category)
+    //     .forEach(c => c['selected'] = false);
 
-  //   category.selected = !category.selected;
-  // }
+    //   category.selected = !category.selected;
+    // }
 
-  openDialog() {
-    this.dialog.open(EditCourseComponent)
-      .afterClosed()
-      .subscribe(result => console.log(result));
-  }
+    openDialog() {
+        this.dialog.open(EditCourseComponent, {
+            data: { courseId: 1 }
+        })
+            .afterClosed()
+            .subscribe(result => console.log(result));
+    }
 }
--- section15/material-demo/src/app/app.module.ts
+++ section15/material-demo/src/app/app.module.ts
@@ -2,45 +2,49 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import {
-  MatCheckboxModule, MatRadioModule, MatSelectModule, MatInputModule,
-  MatDatepickerModule, MatNativeDateModule, MatIconModule, MatButtonModule,
-  MatChipsModule, MatProgressSpinnerModule, MatTooltipModule, MatTabsModule,
-  MatDialogModule
+    MatCheckboxModule, MatRadioModule, MatSelectModule, MatInputModule,
+    MatDatepickerModule, MatNativeDateModule, MatIconModule, MatButtonModule,
+    MatChipsModule, MatProgressSpinnerModule, MatTooltipModule, MatTabsModule,
+    MatDialogModule
 } from '@angular/material';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { EditCourseComponent } from './edit-course/edit-course.component';
+import {
+    EditCourseComponent, // DIALOG_DATA
+} from './edit-course/edit-course.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    EditCourseComponent
-  ],
-  entryComponents: [
-    EditCourseComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatCheckboxModule,
-    MatRadioModule,
-    MatSelectModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatIconModule,
-    MatButtonModule,
-    MatChipsModule,
-    MatProgressSpinnerModule,
-    MatTooltipModule,
-    MatTabsModule,
-    MatDialogModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        EditCourseComponent
+    ],
+    entryComponents: [
+        EditCourseComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatCheckboxModule,
+        MatRadioModule,
+        MatSelectModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatIconModule,
+        MatButtonModule,
+        MatChipsModule,
+        MatProgressSpinnerModule,
+        MatTooltipModule,
+        MatTabsModule,
+        MatDialogModule,
+        FormsModule
+    ],
+    providers: [
+        // { provide: DIALOG_DATA, useValue: {} }
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
