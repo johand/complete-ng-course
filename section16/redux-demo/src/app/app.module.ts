6ae5fe45c52c37b5a73fa508ed534da52270447b
Johan <johand@openmailbox.org>
2018-01-13 01:29:26 -0430

    Installing redux

---
+++ section16/redux-demo/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgRedux, NgReduxModule } from '@angular-redux/store';
+
+import { AppComponent } from './app.component';
+import { IAppState, rootReducer } from './store';
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        NgReduxModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+    constructor(ngRedux: NgRedux<IAppState>) {
+        ngRedux.configureStore(rootReducer, {});
+    }
+}
