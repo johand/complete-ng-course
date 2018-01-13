a89c47b7ce59c63e291ef20907833422e82bca24
Johan <johand@openmailbox.org>
2018-01-13 02:20:21 -0430

    Working with actions

--- section16/redux-demo/src/app/app.component.ts
+++ section16/redux-demo/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { NgRedux } from '@angular-redux/store';
+import { IAppState } from './store';
+import { INCREMENT } from './actions';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title = 'app works!';
+    counter = 0;
+
+    constructor(private ngRedux: NgRedux<IAppState>) {
+
+    }
+
+    increment() {
+        this.ngRedux.dispatch({ type: INCREMENT });
+    }
 }
