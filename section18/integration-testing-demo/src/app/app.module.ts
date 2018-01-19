becc9620bf41eda3fc86a3172e6a97ce715b6d52
Johan <johand@openmailbox.org>
2018-01-19 18:24:04 -0430

    Testing property bindings

--- section18/integration-testing-demo/src/app/app.module.ts
+++ section18/integration-testing-demo/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router'; 
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -12,27 +12,27 @@ import { VoterComponent } from './1-voter/voter.component';
 
 import { routes } from './app.routes';
 import { UsersComponent } from './users/users.component';
-import { NavComponent } from './nav/nav.component';
-import { HighlightDirective } from './highlight.directive'; 
+// import { NavComponent } from './nav/nav.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    TodosComponent,
-    UserDetailsComponent,
-    VoterComponent,
-    UsersComponent,
-    NavComponent,
-    HighlightDirective
-  ],
-  imports: [
-    RouterModule.forRoot(routes),
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        TodosComponent,
+        UserDetailsComponent,
+        VoterComponent,
+        UsersComponent,
+        // NavComponent,
+        HighlightDirective
+    ],
+    imports: [
+        RouterModule.forRoot(routes),
+        BrowserModule,
+        FormsModule,
+        HttpModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
