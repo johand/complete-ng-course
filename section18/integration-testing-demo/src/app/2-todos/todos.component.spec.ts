09c80fd24fe38a3be6b85cdbce533fef9b4a1233
Johan <johand@openmailbox.org>
2018-01-19 21:21:36 -0430

    Providing dependencies

--- section18/integration-testing-demo/src/app/2-todos/todos.component.spec.ts
+++ section18/integration-testing-demo/src/app/2-todos/todos.component.spec.ts
@@ -4,32 +4,36 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { TodosComponent } from './todos.component';
+import { TodoService } from './todo.service';
+import { HttpModule } from '@angular/http';
 
 //NOTE: I've deliberately excluded this suite from running
 // because the test will fail. This is because we have not 
-// provided the TodoService as a dependency to TodosComponent. 
-// 
+// provided the TodoService as a dependency to TodosComponent.
+//
 // When you get to Lecture 6 (Providing Dependencies), be sure
-// to remove "x" from "xdescribe" below. 
+// to remove "x" from "xdescribe" below.
 
-xdescribe('TodosComponent', () => {
-  let component: TodosComponent;
-  let fixture: ComponentFixture<TodosComponent>;
+describe('TodosComponent', () => {
+    let component: TodosComponent;
+    let fixture: ComponentFixture<TodosComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TodosComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [TodosComponent],
+            imports: [HttpModule],
+            providers: [TodoService],
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TodosComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TodosComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
