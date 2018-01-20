a00a7f6cd156de89de1cabf23108eb9002126d0e
Johan <johand@openmailbox.org>
2018-01-19 22:54:13 -0430

    Providing stubs

--- section18/integration-testing-demo/src/app/3-user-details/user-details.component.spec.ts
+++ section18/integration-testing-demo/src/app/3-user-details/user-details.component.spec.ts
@@ -2,27 +2,43 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 import { UserDetailsComponent } from './user-details.component';
 
-xdescribe('UserDetailsComponent', () => {
-  let component: UserDetailsComponent;
-  let fixture: ComponentFixture<UserDetailsComponent>;
+class RouterStub {
+    navigate(params) {
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ UserDetailsComponent ]
-    })
-    .compileComponents();
-  }));
+    }
+}
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UserDetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+class ActivatedRouteStub {
+    params: Observable<any> = Observable.empty();
+}
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+describe('UserDetailsComponent', () => {
+    let component: UserDetailsComponent;
+    let fixture: ComponentFixture<UserDetailsComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [UserDetailsComponent],
+            providers: [
+                { provide: Router, useClass: RouterStub },
+                { provide: ActivatedRoute, useClass: ActivatedRouteStub }
+            ]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(UserDetailsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
