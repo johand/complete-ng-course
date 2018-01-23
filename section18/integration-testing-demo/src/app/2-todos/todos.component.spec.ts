0a9b15e8e8fb2686942b1017a4cdbb6e03bb14ac
Johan <johand@openmailbox.org>
2018-01-23 00:07:45 -0430

    Dealing with asynchronous operations

--- section18/integration-testing-demo/src/app/2-todos/todos.component.spec.ts
+++ section18/integration-testing-demo/src/app/2-todos/todos.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -37,11 +37,14 @@ describe('TodosComponent', () => {
         expect(component).toBeTruthy();
     });
 
-    it('should load todos from the server', () => {
+    it('should load todos from the server', fakeAsync(() => {
         let service = TestBed.get(TodoService);
-        spyOn(service, 'getTodos').and.returnValue(Observable.from([[1, 2, 3]]));
+        spyOn(service, 'getTodosPromise').and.returnValue(Promise.resolve([1, 2, 3]));
 
         fixture.detectChanges();
+
+        tick();
         expect(component.todos.length).toBe(3);
-    });
+
+    }));
 });
