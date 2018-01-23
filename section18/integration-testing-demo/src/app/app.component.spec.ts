ec8b53b07f0da6d54ef8e99dbb83bedd8126eb4c
Johan <johand@openmailbox.org>
2018-01-22 23:23:20 -0430

    Shallow component tests

--- section18/integration-testing-demo/src/app/app.component.spec.ts
+++ section18/integration-testing-demo/src/app/app.component.spec.ts
@@ -2,7 +2,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
@@ -14,7 +14,8 @@ describe('AppComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             imports: [RouterTestingModule.withRoutes([])],
-            declarations: [AppComponent]
+            declarations: [AppComponent],
+            schemas: [NO_ERRORS_SCHEMA]
         })
             .compileComponents();
     }));
@@ -29,14 +30,5 @@ describe('AppComponent', () => {
         let de = fixture.debugElement.query(By.directive(RouterOutlet));
 
         expect(de).not.toBeNull();
-    });
-
-    it('should have a link to todos page', () => {
-        let debugElements = fixture.debugElement.queryAll(By.directive(RouterLinkWithHref));
-
-        // <a href="/todos">
-
-        let index = debugElements.findIndex(de => de.properties['href'] === '/todos');
-        expect(index).toBeGreaterThan(-1);
     });
 });
