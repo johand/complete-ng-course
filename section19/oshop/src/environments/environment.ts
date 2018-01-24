2a778edefe2477699ae80606dcf8e34060fdef2c
Johan <johand@openmailbox.org>
2018-01-24 00:51:57 -0430

    Creating a new project

---
+++ section19/oshop/src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: 'AIzaSyBiNZqrjFdIMFmiP9gvvZFtsH5HGM25P3I',
+        authDomain: 'oshop-d8068.firebaseapp.com',
+        databaseURL: 'https://oshop-d8068.firebaseio.com',
+        projectId: 'oshop-d8068',
+        storageBucket: '',
+        messagingSenderId: '546351441905'
+    }
+};
