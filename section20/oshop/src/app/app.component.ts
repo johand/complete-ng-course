ebfdc64839e3756cddf87422ff197d46207bbadc
Johan <johand@openmailbox.org>
2018-02-07 22:55:13 -0430

    Redirecting after logging in

--- section20/oshop/src/app/app.component.ts
+++ section20/oshop/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    constructor(private auth: AuthService, router: Router) {
+        auth.user$.subscribe(user => {
+            if (user) {
+                let returnUrl = localStorage.getItem('returnUrl');
+                router.navigateByUrl(returnUrl);
+            }
+        });
+    }
 }
