c9a12a8fc1982bc8dc1e0aa366bdd35c5e1e57f3
Johan <johand@openmailbox.org>
2018-02-11 23:19:09 -0430

    Saving the product in firebase

--- section21/oshop/src/app/app.module.ts
+++ section21/oshop/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
@@ -24,6 +25,7 @@ import { UserService } from './user.service';
 import { AdminAuthGuardService } from './admin-auth-guard.service';
 import { ProductFormComponent } from './admin/product-form/product-form.component';
 import { CategoryService } from './category.service';
+import { ProductService } from './product.service';
 
 
 @NgModule({
@@ -42,6 +44,7 @@ import { CategoryService } from './category.service';
         ProductFormComponent
     ],
     imports: [
+        FormsModule,
         BrowserModule,
         AngularFireModule.initializeApp(environment.firebase),
         AngularFireDatabaseModule,
@@ -81,7 +84,8 @@ import { CategoryService } from './category.service';
         AuthGuardService,
         UserService,
         AdminAuthGuardService,
-        CategoryService
+        CategoryService,
+        ProductService
     ],
     bootstrap: [AppComponent]
 })
