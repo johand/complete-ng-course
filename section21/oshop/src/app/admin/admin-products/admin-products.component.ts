f858033a9587c7fe56ed39b51968a54b21770cf8
Johan <johand@openmailbox.org>
2018-02-14 04:54:49 -0430

    Feeding the data table

--- section21/oshop/src/app/admin/admin-products/admin-products.component.ts
+++ section21/oshop/src/app/admin/admin-products/admin-products.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../../product.service';
 import { Subscription } from 'rxjs/Subscription';
 import { Product } from '../../models/product';
+import { DataTableResource } from 'angular5-data-table';
 
 @Component({
     selector: 'app-admin-products',
@@ -12,6 +13,9 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
     products: Product[];
     filteredProducts: Product[];
     subscription: Subscription;
+    tableResource: DataTableResource<Product>;
+    items: Product[] = [];
+    itemCount: number;
 
     constructor(private productService: ProductService) {
         this.subscription = this.productService.getAll()
@@ -20,7 +24,10 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
                     key: action.key, value: action.payload.val()
                 }));
             })
-            .subscribe(products => this.filteredProducts = this.products = products);
+            .subscribe(products => {
+                this.filteredProducts = this.products = products;
+                this.initializeTable(products);
+            });
     }
 
     ngOnInit() {
@@ -30,9 +37,24 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
         this.subscription.unsubscribe();
     }
 
+    private initializeTable(products: Product[]) {
+        this.tableResource = new DataTableResource(products);
+        this.tableResource.query({ offset: 0 })
+            .then(items => this.items = items);
+        this.tableResource.count()
+            .then(count => this.itemCount = count);
+    }
+
     filter(query: string) {
         this.filteredProducts = (query) ?
             this.products.filter(p =>
                 p.value.title.toLowerCase().includes(query.toLowerCase())) : this.products;
+    }
+
+    reloadItems(params) {
+        if (!this.tableResource) { return; }
+
+        this.tableResource.query(params)
+            .then(items => this.items = items);
     }
 }
