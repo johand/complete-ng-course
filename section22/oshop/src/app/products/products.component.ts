bcc60aa878faf676a5eff8e5a059b8e0a00d14f3
Johan <johand@openmailbox.org>
2018-02-15 00:32:44 -0430

    Filtering products by category

--- section22/oshop/src/app/products/products.component.ts
+++ section22/oshop/src/app/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { CategoryService } from '../category.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../models/product';
 
 @Component({
     selector: 'app-products',
@@ -8,12 +10,33 @@ import { CategoryService } from '../category.service';
     styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-    products$;
+    products: Product[] = [];
+    filteredProducts: Product[] = [];
     categories$;
+    category: string;
 
-    constructor(productService: ProductService, categoryService: CategoryService) {
-        this.products$ = productService.getAll();
+    constructor(
+        productService: ProductService,
+        categoryService: CategoryService,
+        route: ActivatedRoute) {
+
+        productService.getAll()
+            .map(actions => {
+                return actions.map(action => ({
+                    key: action.key, value: action.payload.val()
+                }));
+            })
+            .subscribe(products => this.products = products);
+
         this.categories$ = categoryService.getAll();
+
+        route.queryParamMap.subscribe(params => {
+            this.category = params.get('category');
+
+            this.filteredProducts = (this.category) ?
+                this.products.filter(p => p.value.category === this.category) :
+                this.products;
+        });
     }
 
     ngOnInit() {
