b8617e58d3fb9c3c0cf0d677ab22575a40e1b6f4
Johan <johand@openmailbox.org>
2018-03-06 17:43:38 -0430

    Clearing the shopping cart

--- section23/oshop/src/app/shopping-cart.service.ts
+++ section23/oshop/src/app/shopping-cart.service.ts
@@ -11,12 +11,6 @@ export class ShoppingCartService {
 
     constructor(private db: AngularFireDatabase) { }
 
-    private create() {
-        return this.db.list('/shopping-carts').push({
-            dateCreated: new Date().getTime()
-        });
-    }
-
     async getCart(): Promise<Observable<ShoppingCart>> {
         let cartId = await this.getOrCreateCartId();
 
@@ -34,6 +28,25 @@ export class ShoppingCartService {
             });
     }
 
+    async addToCart(product: Product) {
+        this.updateItem(product, 1);
+    }
+
+    async removeFromCart(product: Product) {
+        this.updateItem(product, -1);
+    }
+
+    async clearCart() {
+        let cartId = await this.getOrCreateCartId();
+        this.db.object('/shopping-carts/' + cartId + '/items').remove();
+    }
+
+    private create() {
+        return this.db.list('/shopping-carts').push({
+            dateCreated: new Date().getTime()
+        });
+    }
+
     private getItem(cartId: string, productId: string) {
         return this.db.object('/shopping-carts/' + cartId + '/items/' + productId);
     }
@@ -45,14 +58,6 @@ export class ShoppingCartService {
         let result = await this.create();
         localStorage.setItem('cartId', result.key);
         return result.key;
-    }
-
-    async addToCart(product: Product) {
-        this.updateItem(product, 1);
-    }
-
-    async removeFromCart(product: Product) {
-        this.updateItem(product, -1);
     }
 
     private async updateItem(product: Product, change: number) {
